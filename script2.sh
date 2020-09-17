7b58fa39817eabe9a2cda8fdc0965479bd89d3bf
CS <62691147+panrajucs@users.noreply.github.com>
2020-09-17 16:23:31 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -5,6 +5,4 @@ chmod 664 /var/run/docker.sock
 chmod 777 /var/run/docker.sock
 sudo service jenkins restart
 sleep '10s'
-su - jenkins
-docker login --username="padrajucs" --password="India123*" 
 docker -H tcp://10.1.1.200:2375 run --rm -dit --name jenkins2 --hostname jenkins2 -p 9000:80 sreeharshav/rollingupdate:v5
