964cc71b70086840e5d79968cde63e07fccb5b5d
CS <62691147+panrajucs@users.noreply.github.com>
2020-09-25 11:37:30 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -4,5 +4,5 @@ usermod -aG root jenkins
 chmod 664 /var/run/docker.sock
 chmod 777 /var/run/docker.sock
 sudo service jenkins restart
-sleep '10s'
+sleep '30s'
 docker -H tcp://10.1.1.200:2375 run --rm -dit --name jenkins2 --hostname jenkins2 -p 9000:80 sreeharshav/rollingupdate:v5
